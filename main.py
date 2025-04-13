e4f814ded2babe5250fd6ecd54f7158914ee1bec
Guy Taggar <guypago@outlook.com>
2025-04-13 12:26:35 +0300

    refactor

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from config import OPENAI_KEY
 from services.chatbot_service import Chatbot
 from services.neural_search_service import NeuralSearcher
 
+OUTDATED_QUERY = "Your query has been updated. Please wait for the latest answer."
+
 def get_user_id_from_header(credentials: HTTPAuthorizationCredentials = Depends(HTTPBearer())) -> str:
     """
     Retrieves the user_id from the Authorization header.
@@ -20,11 +22,11 @@ def get_user_id_from_header(credentials: HTTPAuthorizationCredentials = Depends(
         raise HTTPException(status_code=401, detail="User ID missing in Authorization header")
     return user_id
 
-def handle_pending_messages(user_id: str):
+def has_pending_messages(user_id: str):
     if user_pending_messages[user_id] > 0:
         user_pending_messages[user_id] -= 1
-        return "Your query has been updated. Please wait for the latest answer."
-    return None
+        return True
+    return False
 
 app = FastAPI(dependencies=[Depends(get_user_id_from_header)])
 
@@ -52,19 +54,16 @@ async def query(query: Query, user_id: str = Depends(get_user_id_from_header)):
 
     if len(user_history[user_id]) > 0:
         user_message = await Chatbot.build_user_message(openai_client, user_message, list(user_history[user_id]))
-        pending_response = handle_pending_messages(user_id)
-        if pending_response:
-            return {"output": pending_response}
+        if has_pending_messages(user_id):
+            return {"output": OUTDATED_QUERY}
 
     retrieved_data = await neural_searcher.search(text=user_message)
-    pending_response = handle_pending_messages(user_id)
-    if pending_response:
-        return {"output": pending_response}
+    if has_pending_messages(user_id):
+        return {"output": OUTDATED_QUERY}
 
     output = await Chatbot.search(openai_client, retrieved_data, user_message)
-    pending_response = handle_pending_messages(user_id)
-    if pending_response:
-        return {"output": pending_response}
+    if has_pending_messages(user_id):
+        return {"output": OUTDATED_QUERY}
 
     user_pending_messages.pop(user_id)
     user_history[user_id].append({"role": "assistant", "content": output})
